e709a7bde26070060eafad9573c527aea5b3a0e0
Galilea Bernal <galilea.bernal98@gmail.com>
2020-06-27 20:29:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+katz_deli = []
+
+def line(array)
+  if array.length == 0 
+    puts "The line is currently empty."
+  else 
+    constant  = "The line is currently: "
+    array.each_with_index do |name, i|
+    
+  end 
+end
+
+def take_a_number(current_line, name)
+  current_line << name
+   puts "Welcome, #{name}. You are number #{current_line.length} in line."
+end
+
+def now_serving
+
+end
